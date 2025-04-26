3079f3a106088203fa28ecc92c8b16a94c69a3d7
Yixuehahaha <muqing49@163.com>
2025-04-26 15:51:47 -0700

    Small Adjustment

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 from schemas.user_input import UserInput
 from agents.critic_agent import critic_agent
 from agents.rewrite_agent import rewrite_agent
